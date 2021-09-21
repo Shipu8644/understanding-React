3d8e21d270678826605acd68d2b99b8f3e0bd1fb
Shipu8644 <shipu.monjurur@gmail.com>
2021-09-22 00:21:36 +0600

    book

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 import Device from './components/Device/Device';
+import Books from './components/Books/Books';
 
 function App() {
+  const [steps, setSteps] = useState(0);
+  const books = [
+    { name: 'Small Giants', author: 'bo' },
+    { name: 'Build to sell', author: 'john' },
+    { name: 'Grind it', author: 'Ray' },
+    { name: 'Shoe Dog', author: 'bo' },
+  ]
+  const handleIncreaseSteps = () => {
+    setSteps(steps + 1);
+
+  }
+  useEffect(() => {
+    console.log(steps);
+  }, [steps])
   return (
     <div className="App">
-      <Device name='phone' price='16500' />
+      <h3>My steps: {steps}</h3>
+      <button onClick={handleIncreaseSteps}>Walk</button>
+      <Device name='phone' steps={steps} price='16500' />
+      <Books books={books} />
     </div>
   );
 }
--- src/components/Device/Device.js
+++ src/components/Device/Device.js
@@ -3,9 +3,10 @@ import Config from '../Config/Config';
 
 const Device = (props) => {
     return (
-        <div>
+        <div style={{ border: '2px solid green', margin: '5px' }}>
             <h1>My Device: {props.name}</h1>
-            <Config price={props.price} />
+            <h4>Current steps: {props.steps}</h4>
+            <Config price={props.price} steps={props.steps} />
         </div>
     );
 };
